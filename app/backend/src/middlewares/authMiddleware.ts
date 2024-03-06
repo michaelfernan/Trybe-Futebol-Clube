73f2c064565c380ef8bd6e668f49e36eda9b6178
Michael Fernandes <111091339+michaelfernan@users.noreply.github.com>
2024-03-05 21:03:55 -0300

    Add auth middleware and validation to login route

--- app/backend/src/middlewares/authMiddleware.ts
+++ app/backend/src/middlewares/authMiddleware.ts
@@ -2,18 +2,25 @@ import { Request, Response, NextFunction } from 'express';
 import * as jwt from 'jsonwebtoken';
 
 const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
-  const token = req.headers.authorization?.split(' ')[1];
-  if (!token) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).json({ message: 'Token not found' });
+  }
+  const parts = authHeader.split(' ');
+
+  const [scheme, token] = parts;
+
+  if (!/^Bearer$/i.test(scheme)) {
     return res.status(401).json({ message: 'Token must be a valid token' });
   }
 
-  try {
-    const decoded = jwt.verify(token, 'MKF');
+  jwt.verify(token, 'MKF', (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: 'Token must be a valid token' });
+    }
     res.locals.user = decoded;
     next();
-  } catch (error) {
-    return res.status(401).json({ message: 'Token must be a valid token' });
-  }
+  });
 };
 
 export default authMiddleware;
